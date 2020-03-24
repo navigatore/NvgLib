c135a0f8b1809ff411e5e09e0d466d2918064f85
Wojciech Chlapek <wojciech.chlapek@tlen.pl>
2020-03-24 19:05:22 +0100

    Add simple stopwatch class

--- nvg.h
+++ nvg.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <algorithm>
+#include <chrono>
 #include <random>
 
 namespace nvg {
 class Random {
  public:
   template <typename T>
-  std::vector<T> uniformVector(std::size_t n) {
+  [[nodiscard]] std::vector<T> uniformVector(std::size_t n) {
     auto output = std::vector<T>(n, 0);
     auto maxElement = static_cast<T>(n - 1);
     std::uniform_int_distribution<T> dis(0, maxElement);
@@ -17,5 +18,27 @@ class Random {
 
  private:
   std::mt19937 gen{std::random_device{}()};
+};
+
+class Stopwatch {
+ public:
+  void start() noexcept {
+    startTimePoint = std::chrono::high_resolution_clock::now();
+  }
+  void stop() noexcept {
+    auto stopTimePoint = std::chrono::high_resolution_clock::now();
+    duration += stopTimePoint - startTimePoint;
+  }
+  void reset() noexcept {
+    duration = std::chrono::high_resolution_clock::duration::zero();
+  }
+  [[nodiscard]] auto getDurationInMs() const noexcept {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(duration)
+        .count();
+  }
+
+ private:
+  std::chrono::high_resolution_clock::time_point startTimePoint{};
+  std::chrono::high_resolution_clock::duration duration{};
 };
 }  // namespace nvg
